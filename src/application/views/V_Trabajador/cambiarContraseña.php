c52c1f82a7abbd46201ed4f0d01ba607c04577cd
Manu <manuel0311@gmail.com>
2021-06-05 23:59:32 +0200

    Versión 7.1

--- src/application/views/V_Trabajador/cambiarContraseña.php
+++ src/application/views/V_Trabajador/cambiarContraseña.php
@@ -35,7 +35,7 @@ echo '
     </form>
 
   </div>
-</div>
+</div> 
 
 <!--Script -->
     <script src="https://code.jquery.com/jquery-3.3.1.js"integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="crossorigin="anonymous"></script>
