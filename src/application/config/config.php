f172367b553ea8100bc2f334cc99370f194326a8
manuel0311 <61272184+manuel0311@users.noreply.github.com>
2022-09-27 11:04:23 +0200

    cambio  home

--- src/application/config/config.php
+++ src/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'C:\xampp\htdocs\Higea\Manuel_Antonio_De_La_O_Perez\src';
+$config['base_url'] = 'C:/xampp/htdocs/Higea/Manuel_Antonio_De_La_O_Perez/src/';
 
 /*
 |--------------------------------------------------------------------------
